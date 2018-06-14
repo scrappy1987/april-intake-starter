1cdaedd84468945f7c7222cefed9c6bb66f5eea1
Robert12222 <robert.james14@googlemail.com>
2018-06-14 14:05:52 +0100

    Import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO aCCOUNT (id, firstName, SecondName, accountNumber) VALUES (1, 'John', 'Doe', '1234');
+INSERT INTO aCCOUNT (id, firstName, SecondName, accountNumber) VALUES (1, 'James', 'Tom', '1235');
+INSERT INTO aCCOUNT (id, firstName, SecondName, accountNumber) VALUES (1, 'Alex', 'Doe', '1236');
