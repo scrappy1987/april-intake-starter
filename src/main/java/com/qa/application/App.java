763e11a5ae986d826f87b03fef1fc9fa36959e8d
Daniel Smith <daniel.smith@academytrainee.com>
2018-06-19 10:17:08 +0100

    extended from application

--- src/main/java/com/qa/application/App.java
+++ src/main/java/com/qa/application/App.java
@@ -1,20 +1,22 @@
 package com.qa.application;
 
+import javax.ws.rs.core.Application;
+
 import com.qa.domain.Account;
 import com.qa.service.DefaultAccountService;
 import com.qa.util.JSONUtil;
 
-public class App {
+public class App extends Application{
 
 	public static void main(String[] args) {
-		DefaultAccountService service = new DefaultAccountService();
-		JSONUtil util = new JSONUtil();
-		Account joeBloggs = new Account("Joe", "Bloggs", "1234");
-		Account janeBloggs = new Account("Jane", "Bloggs", "1234");
-		service.addAccountFromMap(joeBloggs);
-		service.addAccountFromMap(janeBloggs);
-		String mapAsJSON = util.getJSONForObject(service.getAccountMap());
-		System.out.println("This is the account map as JSON " + mapAsJSON);
+//		DefaultAccountService service = new DefaultAccountService();
+//		JSONUtil util = new JSONUtil();
+//		Account joeBloggs = new Account("Joe", "Bloggs", "1234");
+//		Account janeBloggs = new Account("Jane", "Bloggs", "1234");
+//		service.addAccountFromMap(joeBloggs);
+//		service.addAccountFromMap(janeBloggs);
+//		String mapAsJSON = util.getJSONForObject(service.getAccountMap());
+//		System.out.println("This is the account map as JSON " + mapAsJSON);
 
 	}
 
